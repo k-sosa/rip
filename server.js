b1a593f21e2fe2d1ac4062e39e2b1c6f52debb5b
Austen Haymond <ahaymon17@gmail.com>
2020-03-07 10:21:43 -0700

    created get and post routes for user and post database tables

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.json());
 
 // Routes
 
-// require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
 
 
 // Syncing our sequelize models and then starting our Express app
