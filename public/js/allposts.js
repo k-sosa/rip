c2aaef9cddc0a01c231e062037f70520f6e65e6a
Karen Montes <karen.montes04@gmail.com>
2020-03-12 20:20:44 -0700

    made user input an object to send to front end

--- public/js/allposts.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    url: 'https://developers.zomato.com/api/v2.1/locations?query=' + cityName,
-    method: "GET",
-    headers: {
-        'user-key': 'b5075be7d6cf56502c175fb9e26c2396'
-    }
-}).then(function (response) {
-    console.log(response);
-})
--- public/js/post.js
+++ public/js/post.js
@@ -32,7 +32,7 @@ function handleFormSubmit(event) {
   }
 
   function submitPost(post) {
-    $.post("/api/posts", post, function() {
+    $.post("/api/posts", post, function(object1) {
       window.location.href = "/allposts";
     });
   }
