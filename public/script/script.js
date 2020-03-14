2fe2e81c9cf00d8bd51e5ee4962a330ae2d22da1
Karen Montes <karen.montes04@gmail.com>
2020-03-14 04:54:25 -0700

    seperated javascript from html page

--- public/script/script.js
+++ public/script/script.js
@@ -2,3 +2,32 @@
 function changeBackground() {
     document.body.style.backgroundImage ="url('images/starsbackground.jpg')"
 }
+
+function myFunction() {
+    const Name = $('#searchBar').val().trim();
+
+
+    $.ajax({
+        url: "https://public.opendatasoft.com/api/records/1.0/search/?dataset=namus-missings&q="+Name+"&facet=cityoflastcontact&facet=countydisplaynameoflastcontact&facet=raceethnicity&facet=statedisplaynameoflastcontact&facet=gender",
+        method: "GET"
+        
+    }).then(function (response) {
+        
+        const record1 = response.records[0]
+        const record2 = response.records[0].fields.cityoflastcontact
+        const record3 = response.records[0].fields.currentagefrom
+    
+        console.log(record1)
+        console.log("where they were last seen " + record2)
+        console.log("their current age " + record3)
+    
+       
+        $("#search-div").append('They were last seen in ' +record2 + ' and their current age should be ' + record3 + ' Chat with others who are hoping to find someone.');
+    
+        
+    });    
+};
+
+
+
+
